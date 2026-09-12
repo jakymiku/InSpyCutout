2f84dd0fbd2e44ff65828a8659e7cf8bf293c801
JAKYMIKU <24462148+jakymiku@users.noreply.github.com>
2026-09-13 00:15:11 +0900

    Route runtime caches to app folder

--- Launch_GUI.bat
+++ Launch_GUI.bat
@@ -2,6 +2,14 @@
 setlocal EnableExtensions
 cd /d "%~dp0"
 
+rem Keep model/package caches local to the InSpyCutout folder.
+set "PIP_CACHE_DIR=%~dp0cache\pip"
+set "TRANSPARENT_BACKGROUND_FILE_PATH=%~dp0cache\models"
+set "TORCH_HOME=%~dp0cache\torch"
+set "TORCH_EXTENSIONS_DIR=%~dp0cache\torch_extensions"
+set "HF_HOME=%~dp0cache\huggingface"
+set "XDG_CACHE_HOME=%~dp0cache\xdg"
+
 if not exist ".venv\Scripts\pythonw.exe" (
   echo InSpyCutout environment is not installed yet.
   echo Starting setup...
